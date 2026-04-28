cd6abe491a08b8e85e9ae901568d5ec8fa829ea7
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-28 10:29:51 +0530

    Refactor EmailService to use console logging

--- src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
+++ src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
@@ -1,41 +1,19 @@
 package com.klu.citizen_connect_backend.service;
 
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendOtpEmail(String toEmail, String username, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Citizen Connect - OTP Verification");
-        message.setText(
-                "Hello " + username + ",\n\n" +
-                "Your OTP for Citizen Connect registration is: " + otp + "\n\n" +
-                "This OTP is valid for 5 minutes.\n\n" +
-                "Regards,\nCitizen Connect Team"
-        );
-        mailSender.send(message);
+    public boolean sendOtpEmail(String toEmail, String username, String otp) {
+        System.out.println("=================================");
+        System.out.println("OTP for " + toEmail + " is: " + otp);
+        System.out.println("=================================");
+        return true;
     }
 
-    public void sendLoginEmail(String toEmail, String username) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Citizen Connect - Login Alert");
-        message.setText(
-                "Hello " + username + ",\n\n" +
-                "Your account was logged in successfully.\n\n" +
-                "If this was not you, please reset your password immediately.\n\n" +
-                "Regards,\nCitizen Connect Team"
-        );
-        mailSender.send(message);
+    public boolean sendLoginEmail(String toEmail, String username) {
+        System.out.println("Login email skipped for: " + toEmail);
+        return true;
     }
 }

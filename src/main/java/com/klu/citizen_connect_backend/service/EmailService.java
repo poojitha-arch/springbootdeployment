c6a5bf7824e8927beafc0f67b9c869b6e179119d
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-28 11:53:44 +0530

    Update EmailService.java

--- src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
+++ src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
@@ -6,18 +6,10 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     public boolean sendOtpEmail(String toEmail, String username, String otp) {
-        System.out.println("======================================");
-        System.out.println("OTP GENERATED");
-        System.out.println("Email: " + toEmail);
-        System.out.println("Username: " + username);
-        System.out.println("OTP: " + otp);
-        System.out.println("======================================");
-
         return true;
     }
 
     public boolean sendLoginEmail(String toEmail, String username) {
-        System.out.println("Login email skipped for: " + toEmail);
         return true;
     }
 }

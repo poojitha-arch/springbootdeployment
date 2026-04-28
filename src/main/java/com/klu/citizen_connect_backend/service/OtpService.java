1c322a54256776d7ab1c0be84646b3626b455372
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-28 11:50:56 +0530

    Update OtpService.java

--- src/main/java/com/klu/citizen_connect_backend/service/OtpService.java
+++ src/main/java/com/klu/citizen_connect_backend/service/OtpService.java
@@ -33,12 +33,6 @@ public class OtpService {
         return String.valueOf(otp);
     }
 
-    System.out.println("======================================");
-System.out.println("OTP GENERATED");
-System.out.println("Email: " + request.getEmail());
-System.out.println("Username: " + request.getUsername());
-System.out.println("OTP: " + otp);
-System.out.println("======================================");
 
 emailService.sendOtpEmail(request.getEmail(), request.getUsername(), otp);
 
